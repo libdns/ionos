179e0105fe49a633eb2b94d2df1ab679ce2dd3eb
Jan Delgado <jdelgado@gmx.net>
2025-04-16 12:15:56 +0200

    initial support for libdns 1.0 spec

--- client.go
+++ client.go
@@ -9,6 +9,7 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"os"
 )
 
 const (
@@ -64,33 +65,46 @@ func ionosTTL(ttl float64) *int {
 	return intTTL
 }
 
+func debug(s string) {
+	if os.Getenv("LIBDNS_IONOS_DEBUG") != "" {
+		fmt.Printf("libdns-ionos: %s\n", s)
+	}
+}
+
 func doRequest(token string, request *http.Request) ([]byte, error) {
-	request.Header.Add("X-API-Key", token)
 	request.Header.Add("Content-Type", "application/json")
+	debug(fmt.Sprintf("HTTP req: %+v", request))
+	request.Header.Add("X-API-Key", token)
 
 	client := &http.Client{} // no timeout set because request is w/ context
-	//	fmt.Printf(">>> HTTP req: %+v\n\n", request)
 	response, err := client.Do(request)
+	debug(fmt.Sprintf("HTTP res: %+v, err=%+v", response, err))
 
-	//	fmt.Printf("<<< HTTP res: %+v\n\n", response)
 	if err != nil {
 		return nil, err
 	}
 	defer response.Body.Close()
 
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("read http response body: %w", err)
+	}
+	debug(fmt.Sprintf("<<< HTTP res-body: %s", body))
+
 	if response.StatusCode < 200 || response.StatusCode >= 300 {
 		return nil, fmt.Errorf("%s (%d)", http.StatusText(response.StatusCode), response.StatusCode)
 	}
-	return io.ReadAll(response.Body)
-
+	return body, nil
 }
 
 // GET /v1/zones
 func ionosGetAllZones(ctx context.Context, token string) (getAllZonesResponse, error) {
 	uri := fmt.Sprintf("%s/zones", APIEndpoint)
 	req, err := http.NewRequestWithContext(ctx, "GET", uri, nil)
+	if err != nil {
+		return getAllZonesResponse{}, err
+	}
 	data, err := doRequest(token, req)
-
 	if err != nil {
 		return getAllZonesResponse{}, err
 	}
@@ -102,8 +116,9 @@ func ionosGetAllZones(ctx context.Context, token string) (getAllZonesResponse, e
 }
 
 // ionosGetZone reads the contents of zone by it's IONOS zoneID, optionally filtering for
-// a specific recordType and recordName.
-// /v1/zones/{zoneId}
+// a specific recordType and recordName (IONOS API allows to filter for name,
+// type, suffix).
+// GET /v1/zones/{zoneId}
 func ionosGetZone(ctx context.Context, token string, zoneID string, recordType, recordName string) (getZoneResponse, error) {
 	u, err := url.Parse(APIEndpoint)
 	if err != nil {
@@ -120,6 +135,9 @@ func ionosGetZone(ctx context.Context, token string, zoneID string, recordType, 
 	u.RawQuery = queryString.Encode()
 
 	req, err := http.NewRequestWithContext(ctx, "GET", u.String(), nil)
+	if err != nil {
+		return getZoneResponse{}, err
+	}
 	data, err := doRequest(token, req)
 	var result getZoneResponse
 	if err != nil {
@@ -147,14 +165,12 @@ func ionosFindRecordsInZone(ctx context.Context, token string, zoneID, typ, name
 // ionosDeleteRecord deletes the given record
 // DELETE /v1/zones/{zoneId}/records/{recordId}
 func ionosDeleteRecord(ctx context.Context, token string, zoneID, id string) error {
-
 	if id == "" {
 		return fmt.Errorf("no record id provided")
 	}
 
 	req, err := http.NewRequestWithContext(ctx, "DELETE",
 		fmt.Sprintf("%s/zones/%s/records/%s", APIEndpoint, zoneID, id), nil)
-
 	if err != nil {
 		return err
 	}
@@ -168,8 +184,8 @@ func ionosCreateRecords(
 	ctx context.Context,
 	token string,
 	zoneID string,
-	records []record) ([]zoneRecord, error) {
-
+	records []record,
+) ([]zoneRecord, error) {
 	reqBuffer, err := json.Marshal(records)
 	if err != nil {
 		return nil, err
@@ -192,7 +208,6 @@ func ionosCreateRecords(
 		return nil, err
 	}
 	return zoneRecords, nil
-
 }
 
 // ionosUpdateRecord updates the record with id `id` in the given zone
@@ -211,7 +226,6 @@ func ionosUpdateRecord(ctx context.Context, token string, zoneID, id string, r r
 	req, err := http.NewRequestWithContext(ctx, "PUT",
 		fmt.Sprintf("%s/zones/%s/records/%s", APIEndpoint, zoneID, id),
 		bytes.NewBuffer(reqBuffer))
-
 	if err != nil {
 		return err
 	}
